e5758de19c216a4001815525733735d769bb1274
helloelio <fredwantblossom@gmail.com>
2021-12-16 11:59:46 +0300

    init ts app

---
+++ src/UI/CoockieMessage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="coockie-message">
+    <div class="coockie-message__block-content">
+      <h3 class="coockie-message__title">This website uses cookies.</h3>
+      <p class="coockie-message__test">
+        We use cookies to personalise content and ads, to provide social media
+        features and to analyse our traffic. We also share information about
+        your use of our site with our social media, advertising and analytics
+        partners who may combine it with other information that you’ve provided
+        to them or that they’ve collected from your use of their services.
+      </p>
+    </div>
+    <div class="coockie-message__block-button">
+      <button
+        @click="this.$emit('acceptCoockies')"
+        class="coockie-message__button"
+      >
+        Accept
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CoockieMessage',
+};
+</script>
+
+<style lang="scss" scoped>
+.coockie-message {
+  background-color: #fff;
+  color: black;
+  box-shadow: 0px 0px 10px 0px black;
+  width: 65%;
+  position: fixed;
+  display: grid;
+  justify-content: center;
+  text-align: left;
+  padding: 20px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 1px solid black;
+  border-radius: 8px;
+  margin-bottom: 60px;
+  &__title {
+    margin-bottom: 10px;
+  }
+  &__block-button {
+    justify-self: end;
+  }
+  &__button {
+    background-color: #39a8ef;
+    font-size: 1.1rem;
+    color: white;
+    font-weight: bold;
+    border: 2px solid #fff;
+    padding: 15px 45px;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+  &__button:hover {
+    background-color: #fff;
+    color: #39a8ef;
+    border: 2px solid #39a8ef;
+  }
+}
+</style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="login-block">
+    <form
+      class="login"
+      action="#"
+      @submit.prevent="
+        this.$emit('logging', { name: this.name, password: this.password })
+      "
+    >
+      <label class="login__username" for="username"
+        >Username
+        <input v-model="name" type="text" name="username" required />
+      </label>
+      <label class="login__password" for="password"
+        >Password
+        <input
+          ref="password"
+          v-model="password"
+          type="password"
+          name="password"
+          required
+        />
+        <span
+          v-if="!vissiblePassword"
+          class="vision-button"
+          @click.stop="showPassword"
+        >
+          <img src="@/assets/Vector.svg" alt="" />
+        </span>
+        <span v-else class="vision-button" @click.stop="showPassword">
+          <img src="@/assets/eye-invisible.svg" alt="" />
+        </span>
+      </label>
+      <input class="login__submit" type="submit" value="Login" />
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoginPage',
+  data() {
+    return {
+      name: '',
+      password: '',
+      vissiblePassword: false,
+    };
+  },
+  methods: {
+    showPassword(): void {
+      this.vissiblePassword = this.vissiblePassword ? false : true;
+      if (this.vissiblePassword) {
+        this.$refs.password.type = 'text';
+      } else {
+        this.$refs.password.type = 'password';
+      }
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.login-block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+}
+.login {
+  background-color: #39a8ef;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 1.3rem;
+  max-width: 50%;
+  margin: 0 auto;
+  border: 1px solid black;
+  padding: 30px;
+  &__username {
+    color: #fff;
+    font-weight: bold;
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+    margin-bottom: 15px;
+    width: 12rem;
+  }
+  &__username input {
+    font-size: 1rem;
+    padding: 5px 0;
+    box-shadow: 0px 0px 5px black;
+  }
+  &__password {
+    color: #fff;
+    font-weight: bold;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+    margin-bottom: 15px;
+    width: 12rem;
+  }
+  &__password input {
+    font-size: 1rem;
+    padding: 5px 0;
+    box-shadow: 0px 0px 5px black;
+  }
+  &__submit {
+    background-color: #39a8ef;
+    color: white;
+    font-size: 1rem;
+    font-weight: bold;
+    align-self: center;
+    margin-bottom: 15px;
+    max-width: 12rem;
+    width: 12rem;
+    padding: 10px 0;
+    box-shadow: 0px 0px 5px black;
+    border: 1px solid white;
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+  &__submit:hover {
+    background-color: #fff;
+    color: #39a8ef;
+  }
+}
+.vision-button {
+  position: absolute;
+  background: none;
+  border: none;
+  right: 5px;
+  top: 50%;
+}
+</style>
