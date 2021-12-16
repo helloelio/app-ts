767e4922d4cb8fe4e57ba7c57d45f43760bda9fc
helloelio <fredwantblossom@gmail.com>
2021-12-16 21:54:28 +0300

    add error message for login page

---
+++ src/UI/WrongFields.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrong">
+    <h1>Fields must be field</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'WrongFields',
+});
+</script>
+
+<style lang="scss" scoped>
+.wrong {
+  position: absolute;
+  color: red;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 20px;
+  h1 {
+    background-color: #fff;
+    padding: 10px;
+  }
+}
+</style>
