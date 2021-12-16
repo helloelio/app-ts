a6f0ad306112b6f1968fc3797b80c53fb75647cc
helloelio <fredwantblossom@gmail.com>
2021-12-16 21:54:00 +0300

    refactor

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,65 +1,34 @@
 <template>
+  <WrongFields v-if="validate" />
   <div class="login-block">
-    <form
-      class="login"
-      action="#"
-      @submit.prevent="
-        this.$emit('login', { name: this.name, password: this.password })
-      "
-    >
-      <label class="login__username" for="username"
-        >Username
-        <input v-model="name" type="text" name="username" required />
-      </label>
-      <label class="login__password" for="password"
-        >Password
-        <input
-          ref="password"
-          v-model="password"
-          type="password"
-          name="password"
-          required
-        />
-        <span
-          v-if="!vissiblePassword"
-          class="vision-button"
-          @click.stop="showPassword(this.$refs.password)"
-        >
-          <img src="@/assets/eye-open.svg" alt="" />
-        </span>
-        <span
-          v-else
-          class="vision-button"
-          @click.stop="showPassword(this.$refs.password)"
-        >
-          <img src="@/assets/eye-closed.svg" alt="" />
-        </span>
-      </label>
-      <input class="login__submit" type="submit" value="Login" />
-    </form>
+    <LoginFrom @login="login" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import WrongFields from '@/UI/WrongFields.vue';
+import LoginFrom from '@/components/login/LoginForm.vue';
 
 export default defineComponent({
   name: 'LoginPage',
+  components: {
+    WrongFields,
+    LoginFrom,
+  },
+
   data() {
     return {
-      name: '',
-      password: '',
-      vissiblePassword: false,
+      validate: false,
     };
   },
 
   methods: {
-    showPassword(ref: HTMLInputElement): void {
-      this.vissiblePassword = this.vissiblePassword ? false : true;
-      if (this.vissiblePassword) {
-        ref.type = 'text';
+    login(payload: Record<string, string>): void {
+      if (payload.name === '' || payload.password === '') {
+        this.validate = true;
       } else {
-        ref.type = 'password';
+        this.$emit('login', { name: payload.name, password: payload.password });
       }
     },
   },
@@ -67,73 +36,15 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+
 .login-block {
-  background-color: #184968;
+  background-color: $dark-bg;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   width: 100%;
   height: 100vh;
-}
-.login {
-  &__username {
-    color: #fff;
-    font-weight: bold;
-    display: flex;
-    flex-direction: column;
-    align-self: center;
-    margin-bottom: 15px;
-    width: 12rem;
-  }
-  &__username input {
-    font-size: 1rem;
-    border: none;
-    padding: 5px 5px;
-    box-shadow: 0px 0px 10px black;
-  }
-  &__password {
-    color: #fff;
-    font-weight: bold;
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-self: center;
-    margin-bottom: 15px;
-    width: 12rem;
-  }
-  &__password input {
-    border: none;
-    font-size: 1rem;
-    padding: 5px 5px;
-    box-shadow: 0px 0px 10px black;
-  }
-  &__submit {
-    background-color: #39a8ef;
-    color: white;
-    font-size: 1rem;
-    font-weight: bold;
-    align-self: center;
-    margin-bottom: 15px;
-    max-width: 12rem;
-    width: 12rem;
-    padding: 10px 0;
-    box-shadow: 0px 0px 5px black;
-    border: 1px solid white;
-    cursor: pointer;
-    transition: all 0.3s;
-  }
-  &__submit:hover {
-    background-color: #fff;
-    color: #39a8ef;
-  }
-}
-.vision-button {
-  position: absolute;
-  background: none;
-  border: none;
-  right: 5px;
-  top: 45%;
-  cursor: pointer;
 }
 </style>
