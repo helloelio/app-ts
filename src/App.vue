9ba2bdff34889a4f6ac75689b277d35f3b2842f3
helloelio <fredwantblossom@gmail.com>
2021-12-17 17:35:01 +0300

    chore: Add style for fullscreen height to #app

--- src/App.vue
+++ src/App.vue
@@ -111,6 +111,7 @@ export default defineComponent({
   font-family: 'M PLUS Rounded 1c', sans-serif;
 }
 #app {
+  height: 100vh;
   background-color: rgb(32, 32, 35);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
