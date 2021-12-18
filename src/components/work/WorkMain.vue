3627004b8a426e416a73768376d2d36e721363f1
helloelio <fredwantblossom@gmail.com>
2021-12-18 20:29:43 +0300

    chore: Refactor

--- src/components/work/WorkMain.vue
+++ src/components/work/WorkMain.vue
@@ -14,45 +14,42 @@ export default defineComponent({
   components: {
     WorkItem,
   },
-  data() {
-    return {
-      obj: { name: 'asd', age: 23 },
-      works: [
-        {
-          id: 1,
-          title: 'eCommerce Component',
-          subtitle:
-            'e-Commerce-component, a concept of a store and a cart of products (also for mobile)',
-          img: require('@/assets/images/WorkThree.png'),
-          link: 'https://e-commerce-component.vercel.app/',
-        },
-        {
-          id: 2,
-          title: 'Tip Calculator',
-          subtitle: 'Build with Vue 3, Vite, Tailwind CSS.',
-          img: require('@/assets/images/WorkOne.png'),
-          link: 'https://tip-calculator-roan.vercel.app/',
-        },
-        {
-          id: 3,
-          title: 'Weather App',
-          subtitle: 'Vue weather app with vuex, weatherAPI.',
-          img: require('@/assets/images/WorkTwo.png'),
 
-          link: 'https://vue-weather-app-seven.vercel.app/#/',
-        },
-        {
-          id: 4,
-          title: 'Google Books',
-          subtitle:
-            'Test task for a "future group" for a junior frontend developer. ',
-          img: require('@/assets/images/WorkFour.png'),
+  data: () => ({
+    works: [
+      {
+        id: 1,
+        title: 'eCommerce Component',
+        subtitle:
+          'e-Commerce-component, a concept of a store and a cart of products (also for mobile)',
+        img: require('@/assets/images/WorkThree.png'),
+        link: 'https://e-commerce-component.vercel.app/',
+      },
+      {
+        id: 2,
+        title: 'Tip Calculator',
+        subtitle: 'Build with Vue 3, Vite, Tailwind CSS.',
+        img: require('@/assets/images/WorkOne.png'),
+        link: 'https://tip-calculator-roan.vercel.app/',
+      },
+      {
+        id: 3,
+        title: 'Weather App',
+        subtitle: 'Vue weather app with vuex, weatherAPI.',
+        img: require('@/assets/images/WorkTwo.png'),
 
-          link: 'https://react-google-books-api-ghvdmfaa8-helloelio.vercel.app/',
-        },
-      ],
-    };
-  },
+        link: 'https://vue-weather-app-seven.vercel.app/#/',
+      },
+      {
+        id: 4,
+        title: 'Google Books',
+        subtitle:
+          'Test task for a "future group" for a junior frontend developer. ',
+        img: require('@/assets/images/WorkFour.png'),
+        link: 'https://react-google-books-api-ghvdmfaa8-helloelio.vercel.app/',
+      },
+    ],
+  }),
 });
 </script>
 
