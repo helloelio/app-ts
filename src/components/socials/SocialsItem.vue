56e23a6f790ada85fd634793d52efc02c3732df1
helloelio <fredwantblossom@gmail.com>
2021-12-18 18:36:04 +0300

    feat: Create new components SocialItem, SocialMain

---
+++ src/components/socials/SocialsItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <a :href="social.url" v-for="social in socials" :key="social.id">
+    <div class="socials__item">
+      <div class="socials__image">
+        <img :src="social.img" :alt="social.alt" />
+      </div>
+      <div class="socials__nickname">{{ social.username }}</div>
+    </div>
+  </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SocilasItem',
+  props: {
+    socials: {
+      type: Array,
+      default: () => [],
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+
+a {
+  display: block;
+}
+.socials__item {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  column-gap: 10px;
+  align-items: center;
+  font-size: 2rem;
+  border-radius: 20px;
+  padding: 10px;
+  border: 1px solid white;
+}
+
+.socials__item:hover {
+  border: 1px solid white;
+  box-shadow: 0 0 5px 0 black;
+  transform: scale(1.01);
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(235, 171, 205, 1) 100%
+  );
+}
+
+.socials__nickname {
+  text-decoration: underline;
+}
+.socials__image {
+  display: flex;
+  border-radius: 20px;
+}
+.socials__image img {
+  max-height: 50px;
+}
+</style>
---
+++ src/components/socials/SocialsMain.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2 class="title">Socials <span class="blinked">_</span></h2>
+  <div class="socials">
+    <SocialsItems :socials="socials" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SocialsItems from '@/components/socials/SocialsItem.vue';
+
+export default defineComponent({
+  name: 'SocialsMain',
+  components: {
+    SocialsItems,
+  },
+  data() {
+    return {
+      socials: [
+        {
+          id: 1,
+          img: require('@/assets/images/instagram-social.svg'),
+          alt: 'instagram logo',
+          username: '@madve.dev',
+          url: 'https://www.instagram.com/madve.dev/',
+        },
+        {
+          id: 2,
+          img: require('@/assets/images/github-social.svg'),
+          alt: 'github logo',
+          username: '@helloelio',
+          url: 'https://github.com/helloelio',
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.socials {
+  display: grid;
+  row-gap: 20px;
+}
+</style>
