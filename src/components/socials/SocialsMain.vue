c9604543b0bd09ed839bc2a56c671296943d24f5
helloelio <fredwantblossom@gmail.com>
2021-12-19 18:00:31 +0300

    feat: Refactor to vue 3 setup!!!

--- src/components/socials/SocialsMain.vue
+++ src/components/socials/SocialsMain.vue
@@ -14,25 +14,25 @@ export default defineComponent({
   components: {
     SocialsItems,
   },
-  data() {
-    return {
-      socials: [
-        {
-          id: 1,
-          img: require('@/assets/images/instagram-social.svg'),
-          alt: 'instagram logo',
-          username: '@madve.dev',
-          url: 'https://www.instagram.com/madve.dev/',
-        },
-        {
-          id: 2,
-          img: require('@/assets/images/github-social.svg'),
-          alt: 'github logo',
-          username: '@helloelio',
-          url: 'https://github.com/helloelio',
-        },
-      ],
-    };
+
+  setup() {
+    const socials = [
+      {
+        id: 1,
+        img: require('@/assets/images/instagram-social.svg'),
+        alt: 'instagram logo',
+        username: '@madve.dev',
+        url: 'https://www.instagram.com/madve.dev/',
+      },
+      {
+        id: 2,
+        img: require('@/assets/images/github-social.svg'),
+        alt: 'github logo',
+        username: '@helloelio',
+        url: 'https://github.com/helloelio',
+      },
+    ];
+    return { socials };
   },
 });
 </script>
